70989030fd99f6a2b71ad59c2c80c8ad0ce90385
Chris Wood <chris@myemma.com>
2010-08-15 18:41:51 -0500

    Adding some initial thoughts on measuring engagement.

---
+++ engagement/engagement.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+engagement.py
+
+Created by Chris Wood on 2010-08-15.
+
+"""
+
+import sys
+import os
+import unittest
+
+
+# 
+# 
+
+
+class engagement:
+	def __init__(self):
+		pass
+
+
+class engagementTests(unittest.TestCase):
+	def setUp(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
